5229bfeb99a801098df3c9232c6f61645ef67f8c
Chancelor Assiamah <chanceyassiamah@gmail.com>
2025-11-16 10:07:33 -0500

    API: added an extremely basic test for streaming responses. can confirm it's working.

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
+import asyncio
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
-def main():
-    print("Hello from agent!")
+
+# allows the frontend to make requests to this REST API
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    # TODO: for production, change to be specifically the frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+async def test_streaming_response():
+    yield "Hello from LicenseGuard-Agent! "
+    await asyncio.sleep(1.0)
+    test_str = "Testing if we can stream responses to the frontend... Goodbye!"
+    for word in test_str:
+        yield word
+        await asyncio.sleep(0.1)
+
+
+@app.get("/generate/report")
+async def main():
+    return StreamingResponse(
+        test_streaming_response(),
+        media_type="text/event-stream"
+    )
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
